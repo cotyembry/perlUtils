659de9dbd37fb8c7fc0b9614f9452556a4d3903c
Coty Embry <cotyembry@gmail.com>
2017-04-27 18:45:56 -0500

    even better commit

--- gp.pl
+++ gp.pl
@@ -3,18 +3,26 @@
 
 # if (not defined $argument1) {}
 # if (defined $argument1) {}
-$argument1 = $ARGV[0];				#$argument1 will be the commit message
-
-# system "type search.pl"
+$argument1 = $ARGV[0];				#$argument1 will be the commit message\
+$numberOfArguments = scalar @ARGV;
+$commitMessage = "";
+for($i = 0; $i < $numberOfArguments; $i++) {
+	if($i == 0) {
+		$commitMessage = "$ARGV[$i]";
+	}
+	else {
+		$commitMessage = "$commitMessage $ARGV[$i]";
+	}
+}
 
 if(defined $argument1) {
+	#if here, this means at least one work was passed in to be put as the commit message
 	system "git add -A";
-	system "git commit -m \"$argument1\"";
+	system "git commit -m \"$commitMessage\"";
 	system "git push origin master";
 }
 else {
 	system "git add -A";
 	system "git commit -m \"updating files\"";
 	system "git push origin master";
-	# system "git add -A && git commit \"updating files\" && git push origin master";
 }
